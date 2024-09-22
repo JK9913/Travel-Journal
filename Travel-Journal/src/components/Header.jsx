eaea9f4d04ffede7cd0c85a8b8387df9f9ac504c
Jan-Kristian Karlsen <janke3567@gmail.com>
2024-09-22 22:44:29 +0200

    started styling the header

--- Travel-Journal/src/components/Header.jsx
+++ Travel-Journal/src/components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header() {
     return (
-        <div>
-            <header className="App-header">
-                <img src="../assets/world-svgrepo-com.svg" className="App-logo" alt="logo of the planet earth" />
+        <header className="App-header">
+            <div className="logo-title-header">
+                <img src=".\src\assets\world-svgrepo-com.svg" className="App-logo" alt="logo of the planet earth" />
                 <h1>my travel journal</h1>
-            </header>
-        </div>
+            </div>
+        </header>
     )
 }
