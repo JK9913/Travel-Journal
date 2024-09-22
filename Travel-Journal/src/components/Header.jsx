4429682f754792adaa67c986723eb6e88bf8a448
Jan-Kristian Karlsen <janke3567@gmail.com>
2024-09-22 22:09:29 +0200

    Cleaned up the vite default repo and added a header file

---
+++ Travel-Journal/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <div>
+            <header className="App-header">
+                <img src="../assets/world-svgrepo-com.svg" className="App-logo" alt="logo of the planet earth" />
+                <h1>my travel journal</h1>
+            </header>
+        </div>
+    )
+}
